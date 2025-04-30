5ef8f01f920fed922ae33c15e4fe5cd3c465df0a
boscoli <ickboscoli@gmail.com>
2025-04-30 00:12:35 -0300

    Segundo commit

--- Selenium.py
+++ Selenium.py
@@ -3,63 +3,24 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
-#Abrir o navegador
-
 navegador = webdriver.Chrome()
-
-#Abrir um site
 navegador.get('https://www4.fazenda.sp.gov.br/DareICMS/DareAvulso')
-
-#Deixar em tela cheia
 navegador.maximize_window()
 
-# Selecionar um elemento na tela
 cnpj = navegador.find_element('id', 'txtCriterioConsulta')
-
-#clicar em um elemento
-
 cnpj.click()
-
-#encontrar varios elementos
-#lista_botoes = navegador.find_element('class name', 'form-control')
-
-#for botao in lista_botoes:
- #   if 'cnpj' in botao.text:
-  #      botao.click()
-  #      break
-
-
-#Selecionar aba
-#aba = navegador.window_handles
-#navegador.switch_to.window(aba[0])
-
-#Escrever em um campo/formulario
-#observacao = navegador.find_element('id', 'txtObservacao')
 cnpj.send_keys('44084747000140')
 
 consultar = navegador.find_element('id', 'btnConsultar')
 consultar.click()
-
-#Colocar elemento na tela
-#navegador.execute_script("arguments[0].scrollIntoView({block:'center'})", botaoqueroclicar)
-#botaoqueroclicar = navegador.find_element('id', 'btnConsultar')
-
-#Esperar o carregamento de algo
-#Opçao 1
-#time.sleep(3)
-
-#Opçao 2, espera dinamica
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 time.sleep(8)
+
 select_element = navegador.find_element('id', 'ddlTipoDebito')
 navegador.execute_script("arguments[0].scrollIntoView({block:'center'})", select_element)
-time.sleep(2)
+time.sleep(1)
 select_element.click()
 select = Select(select_element)
-time.sleep(3)
+time.sleep(0.5)
 
 select.select_by_value("6304")
 
@@ -71,16 +32,48 @@ from datetime import datetime
 
 dados = pd.read_excel("Exemplo.xlsx", engine="openpyxl")
 data = dados.iloc[2, 14]
-print("Data:", data)
 data_formatada = pd.to_datetime(data).date()
 dataconvertida = data_formatada.strftime("%d/%m/%Y")
-print("Data formatada:", dataconvertida)
-
-time.sleep(2)
+time.sleep(1)
 
 campodata = navegador.find_element('id', 'txtVencimento')
 campodata.click()
-
+campodata.send_keys(Keys.ARROW_LEFT)
+campodata.send_keys(Keys.ARROW_LEFT)
 campodata.send_keys(dataconvertida)
 
-time.sleep(3)
+time.sleep(1)
+
+mes_ano = data_formatada.strftime("%m/%Y")
+camporef = navegador.find_element("id", 'txtReferencia')
+camporef.click()
+camporef.send_keys(mes_ano)
+
+time.sleep(1)
+
+numNota = dados.iloc[0, 1]
+nomenota = dados.iloc[1, 1]
+valornota = dados.iloc[0, 11]
+
+obs = navegador.find_element("id", "txtObservacao")
+obs.click()
+obs.send_keys(f"ICMS ST ANTEC. NFE - nº{numNota} - {nomenota} ")
+
+time.sleep(0.5)
+
+campovalor = navegador.find_element("id", "txtValorCalculo")
+campovalor.click()
+campovalor.send_keys(f"ICMS ST PAGAR - R${valornota:.2f}")
+
+time.sleep(2)
+
+btngerar = navegador.find_element("id", "btnGerar")
+btngerar.click()
+
+time.sleep(2)
+
+from selenium.webdriver.common.alert import Alert
+alerta = Alert(navegador)
+alerta.accept()
+
+time.sleep(5)
